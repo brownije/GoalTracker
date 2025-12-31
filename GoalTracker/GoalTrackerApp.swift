e4b2e74482e6f71c7113d762ed2847f8e9a2bbc5
Josh Browning <brownije@dukes.jmu.edu>
2025-12-31 14:33:47 -0500

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by Joshua Browning on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
