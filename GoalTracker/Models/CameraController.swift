7eafcf95cf789419aa8eebb2df0f64b37f407088
Josh Browning <brownije@dukes.jmu.edu>
2026-01-13 22:37:12 -0500

    Added shutter button & CameraController class

---
+++ GoalTracker/Models/CameraController.swift
@@ -0,0 +1,118 @@
+//
+//  CameraController.swift
+//  GoalTracker
+//
+//  Created by Joshua Browning on 1/13/26.
+//
+
+import AVFoundation
+import UIKit
+
+class CameraController: UIViewController {
+    // Capture session
+    var session: AVCaptureSession?
+    
+    // Photo  output
+    let output = AVCapturePhotoOutput()
+    
+    // Video preview
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    
+    // Shutter button (camera)
+    private let shutterButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 210))
+        // Looks
+        button.layer.cornerRadius = 50
+        button.layer.borderWidth = 3
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
+        view.addSubview(shutterButton)
+        checkCameraPermissions()
+        
+        shutterButton.addTarget(self, action: #selector(didTakePhoto), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.bounds
+        
+        shutterButton.center = CGPoint(x: view.frame.size.width/2,
+                                       y: view.frame.size.height - 100)
+    }
+    
+    private func checkCameraPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            // Request
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                guard granted else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.setUpCamera( )
+                }
+            }
+        // MARK: TODO: Other cases
+        case .restricted:
+            break
+        case .authorized:
+            setUpCamera()
+        default:
+            break
+        }
+    }
+    
+    private func setUpCamera() {
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input) {
+                    session.addInput(input)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                }
+                
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                
+                session.startRunning()
+                self.session = session
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+    
+    @objc private func didTakePhoto() {
+        output.capturePhoto(with: AVCapturePhotoSettings(),
+                            delegate: self)
+    }
+}
+
+// Turn capture into a UIImage (photo)
+extension CameraController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation() else {
+            return
+        }
+        let image = UIImage(data: data)
+        
+        session?.stopRunning()
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.frame = view.bounds
+        view.addSubview(imageView)
+    }
+}
--- GoalTracker/Views/CompleteGoalView.swift
+++ GoalTracker/Views/CompleteGoalView.swift
@@ -7,9 +7,20 @@
 
 import SwiftUI
 
+// SwiftUI wrapper for CameraController (assumed to be a UIViewController subclass)
+struct CameraControllerView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> CameraController {
+        CameraController()
+    }
+
+    func updateUIViewController(_ uiViewController: CameraController, context: Context) {
+        // Update the view controller if needed
+    }
+}
+
 struct CompleteGoalView: View {
     var body: some View {
-        CameraView()
+        CameraControllerView()
     }
 }
 
