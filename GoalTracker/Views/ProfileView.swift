9fb27c7e587c189cb8788569e1309f969275304b
Josh Browning <brownije@dukes.jmu.edu>
2026-01-14 21:17:49 -0500

    Basic profile view implementation

--- GoalTracker/Views/ProfileView.swift
+++ GoalTracker/Views/ProfileView.swift
@@ -7,4 +7,40 @@
 
 import SwiftUI
 
-
+struct ProfileView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var fullName: String = ""
+    
+    @State private var isLoggedIn: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section(header: Text("Public Information")) {
+                    TextField("Username", text: $username)
+                    TextField("Full Name", text: $fullName)
+                }
+                
+                Section {
+                    Button("Update Profile") {
+                        updateProfile()
+                    }
+                }
+            }
+        }
+        .navigationTitle("Edit Profile")
+        .task {
+            await getInitialProfile()
+        }
+    }
+    func getInitialProfile() async {
+        // Backend handling
+        print("Getting profile...")
+    }
+    
+    func updateProfile() {
+        // MARK: TODO: Add pfp func
+        print("Updating profile...")
+    }
+}
