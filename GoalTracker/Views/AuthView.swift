92136e1e3dce29c0b7a86c7b26efc51b2cd9003d
Josh Browning <brownije@dukes.jmu.edu>
2026-01-17 20:57:06 -0500

    Email field

--- GoalTracker/Views/AuthView.swift
+++ GoalTracker/Views/AuthView.swift
@@ -22,7 +22,7 @@ struct AuthView: View {
           .textContentType(.emailAddress)
           .textInputAutocapitalization(.never)
           .autocorrectionDisabled()
-          // Email 
+          // Email field
       }
         
         Section {
